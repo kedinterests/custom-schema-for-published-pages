7f779c723d1fd8638126917027728ecda676ae95
Chris Malone <chris@m11design.com>
2024-12-04 15:49:27 -0500

    add debugging

--- plugin.rb
+++ plugin.rb
@@ -9,40 +9,41 @@
 
 enabled_site_setting :schema_pages_enabled
 
+Rails.logger.info("Custom Schema Plugin: Initializing...")
+
 after_initialize do
+  Rails.logger.info("Custom Schema Plugin: After initialize block started.")
+
   # Register custom fields
   register_topic_custom_field_type("is_published_page", :boolean)
   register_topic_custom_field_type("schema_type", :string)
 
+  Rails.logger.info("Custom Schema Plugin: Custom fields registered.")
+
   # Add custom schema to TopicViewSerializer
   add_to_serializer(:topic_view, :schema_markup) do
-	next unless object.topic.custom_fields["is_published_page"]
+    Rails.logger.info("Custom Schema Plugin: Adding schema markup to serializer.")
+    next unless object.topic.custom_fields["is_published_page"]
 
-	schema_type = object.topic.custom_fields["schema_type"]
-	template = case schema_type
-			   when "Article"
-				 "schema/article_schema"
-			   when "FAQPage"
-				 "schema/faq_schema"
-			   when "Event"
-				 "schema/event_schema"
-			   when "Directory"
-				 "schema/directory_schema"
-			   else
-				 "schema/default_schema"
-			   end
+    schema_type = object.topic.custom_fields["schema_type"]
+    template = case schema_type
+               when "Article"
+                 "schema/article_schema"
+               when "FAQPage"
+                 "schema/faq_schema"
+               when "Event"
+                 "schema/event_schema"
+               when "Directory"
+                 "schema/directory_schema"
+               else
+                 "schema/default_schema"
+               end
 
-	# Render the appropriate schema template
-	render_template(template, topic: object.topic)
+    # Render the appropriate schema template
+    render_template(template, topic: object.topic)
   end
 
-  # Extend TopicViewSerializer to expose `schema_type` field for admin UI
-  add_to_serializer(:topic_view, :schema_type) do
-	object.topic.custom_fields["schema_type"]
-  end
-
-  # Extend TopicViewSerializer to expose `is_published_page` field for admin UI
-  add_to_serializer(:topic_view, :is_published_page) do
-	object.topic.custom_fields["is_published_page"]
-  end
+  Rails.logger.info("Custom Schema Plugin: Serializer customization complete.")
 end
+
+Rails.logger.info("Custom Schema Plugin: Initialization complete.")
